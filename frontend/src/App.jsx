83ca84478a57bb1c05faf9f1443d79444107d7cc
ritiksharma-code <ritiksharma0405@gmail.com>
2025-02-25 19:59:28 +0530

    added settings(theme) page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,20 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
 import {Loader} from "lucide-react"
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
 
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log({ authUser });
+  // console.log({ authUser });
 
   if(isCheckingAuth && !authUser) return (
     <div className='flex items-center justify-center h-screen'>
@@ -27,7 +29,7 @@ const App = () => {
   )
 
   return (
-    <div>
+    <div data-theme={theme}>
 
       <Navbar />
 
