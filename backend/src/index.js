5384cd5328fddc8f302660faa20b58af8fd277a3
ritiksharma-code <ritiksharma0405@gmail.com>
2025-02-27 22:30:47 +0530

    made changes for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -13,6 +15,7 @@ dotenv.config();
 // const app  = express();          // don't need this now, importing from socket.js
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());        // used to create JSON web token (JWT) to extract data from user in JSON format
 app.use(cookieParser());
@@ -23,6 +26,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 /* app.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT);
